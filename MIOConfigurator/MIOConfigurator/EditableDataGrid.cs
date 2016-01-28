bd0e7554eb1e4123c63d8ccca226ebd5c5028635
toschkin <toshkin@bk.ru>
2016-01-28 15:18:22 +0200

    grid editing when errors present

---
+++ MIOConfigurator/MIOConfigurator/EditableDataGrid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MIOConfigurator
+{
+    public class EditableDataGrid : DataGrid
+    {
+        protected override void OnCanExecuteBeginEdit(CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+            e.Handled = true;
+        }
+    }
+}
--- MIOConfigurator/MIOConfigurator/MainWindow.xaml.cs
+++ MIOConfigurator/MIOConfigurator/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Text.RegularExpressions;
